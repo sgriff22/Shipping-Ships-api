18a7a5988533fc3e81ec0e0bf926524f3b26ed9c
Stacy Griffy <sgriffy2@gmail.com>
2024-02-26 16:41:18 -0600

    Added POST functionality and defined a create function for docks, haulers, and ships

--- views/dock_view.py
+++ views/dock_view.py
@@ -80,3 +80,19 @@ def retrieve_dock(pk):
         serialized_dock = json.dumps(dict(query_results))
 
     return serialized_dock
+
+def create_dock(dock_data):
+    with sqlite3.connect("./shipping.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute(
+            """
+            INSERT INTO Dock (location, capacity)
+            VALUES (?, ?)
+            """,
+            (dock_data["location"], dock_data["capacity"]),
+        )
+
+        new_dock_id = db_cursor.lastrowid
+
+    return new_dock_id if new_dock_id is not None else None
